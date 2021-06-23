4a65de7d59a64158c351a9e7fc81fdedd197cb64
walstanb <walstanb@gmail.com>
2021-06-23 17:10:39 +0530

    feat: fields for Discount Levels

--- rasiin/install.py
+++ rasiin/install.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
 
 import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+
 
 def after_install():
-		make_custom_roles()
+	make_custom_roles()
+	make_custom_fields()
 
 def make_custom_roles():
 	for role_name in ['Accounting Manager']:
@@ -18,3 +21,25 @@ def make_custom_roles():
 			pass
 
 	frappe.db.commit()
+
+
+def make_custom_fields():
+	create_custom_fields(custom_fields)
+	frappe.db.commit()
+
+custom_fields = {
+	'Selling Settings': [
+		{
+			'fieldname': 'discount_levels_section',
+			'fieldtype': 'Section Break',
+			'label': 'Discount Levels',
+			'insert_after': 'hide_tax_id',
+		},
+		{
+			'fieldname': 'discount_levels',
+			'fieldtype': 'Table',
+			'options': 'Discount Level',
+			'insert_after': 'hide'
+		}
+	],
+}
