f5923c6eb065510f8399fcbba97471cd8155ba2a
Sagar Vora <sagar@resilient.tech>
2021-06-28 15:21:18 +0530

    port custom order for standard fields

--- rasiin/install.py
+++ rasiin/install.py
@@ -2,11 +2,12 @@ from __future__ import unicode_literals
 
 import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
-
+from frappe.custom.doctype.property_setter.property_setter import make_property_setter
 
 def after_install():
 	make_custom_roles()
 	make_custom_fields()
+	make_property_setters()
 
 def make_custom_roles():
 	for role_name in ['Accounting Manager']:
@@ -26,6 +27,21 @@ def make_custom_roles():
 def make_custom_fields():
 	create_custom_fields(custom_fields)
 	frappe.db.commit()
+
+def make_property_setters():
+	for doctype, fields in insert_after_map.items():
+		for fieldname, previous_field in fields.items():
+			make_property_setter(doctype, fieldname, "is_custom_field", 1, "")
+			make_property_setter(doctype, fieldname, "insert_after", previous_field, "")
+
+	frappe.db.commit()
+
+insert_after_map = {
+	'Sales Invoice': {
+		'customer_group': 'customer_name',
+		'payments': 'total_qty'
+	}
+}
 
 custom_fields = {
 	'Selling Settings': [
