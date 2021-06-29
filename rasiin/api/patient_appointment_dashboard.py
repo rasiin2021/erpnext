4cc0bd64ca2d84eef725742d1cc8d2c0eb5f2d13
Sagar Vora <sagar@resilient.tech>
2021-06-30 00:15:30 +0530

    fix: dashboards

--- rasiin/api/patient_appointment_dashboard.py
+++ rasiin/api/patient_appointment_dashboard.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from frappe import _
 
-def get_data():
+def get_data(data):
 	return {
 		'fieldname': 'appointment',
 		'non_standard_fieldnames': {
--- rasiin/api/patient_dashboard.py
+++ rasiin/api/patient_dashboard.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from frappe import _
 
-def get_data():
+def get_data(data):
 	return {
 		'heatmap': True,
 		'heatmap_message': _('This is based on transactions against this Patient. See timeline below for details'),
